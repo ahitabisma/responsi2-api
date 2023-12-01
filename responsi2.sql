743c151eb99073a25eb2c0902bbb0f5038cba3ea
ahitabisma <ahitabisma21@gmail.com>
2023-12-01 21:24:10 +0700

    DB

--- responsi2.sql
+++ responsi2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2023 at 03:09 PM
+-- Generation Time: Dec 01, 2023 at 03:23 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.1.25
 
@@ -29,7 +29,6 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `tamu` (
   `id` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
   `nama` varchar(255) DEFAULT NULL,
   `alamat` varchar(255) DEFAULT NULL,
   `nomor_telepon` varchar(15) DEFAULT NULL,
@@ -43,9 +42,9 @@ CREATE TABLE `tamu` (
 -- Dumping data for table `tamu`
 --
 
-INSERT INTO `tamu` (`id`, `user_id`, `nama`, `alamat`, `nomor_telepon`, `tanggal_kunjungan`, `keperluan`, `created_at`, `updated_at`) VALUES
-(1, 1, 'Ahita Bisma', 'Purwokerto', '082223334566', '2023-12-01', 'Silaturahim', '2023-12-01 10:47:34', '2023-12-01 10:47:34'),
-(2, NULL, 'Bisma', 'Bobosan', '82225405600', '2023-12-01', 'Bermain', '2023-12-01 11:58:10', '2023-12-01 13:48:45');
+INSERT INTO `tamu` (`id`, `nama`, `alamat`, `nomor_telepon`, `tanggal_kunjungan`, `keperluan`, `created_at`, `updated_at`) VALUES
+(1, 'Ahita Bisma', 'Purwokerto', '082223334566', '2023-12-01', 'Silaturahim', '2023-12-01 03:47:34', '2023-12-01 03:47:34'),
+(2, 'Bisma', 'Bobosan', '82225405600', '2023-12-01', 'Bermain', '2023-12-01 04:58:10', '2023-12-01 06:48:45');
 
 -- --------------------------------------------------------
 
@@ -66,49 +65,6 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `username`, `password`) VALUES
 (1, 'bisma', 'e10adc3949ba59abbe56e057f20f883e'),
 (2, 'lula', 'e10adc3949ba59abbe56e057f20f883e');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tamu`
---
-ALTER TABLE `tamu`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tamu`
---
-ALTER TABLE `tamu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `tamu`
---
-ALTER TABLE `tamu`
-  ADD CONSTRAINT `tamu_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
